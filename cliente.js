46c94a1827cef0e2363b3b5d3a3112e52ae02f41
Giuliano Marinelli <giuliano.marinelli@hotmail.com>
2018-06-28 17:43:25 -0300

    Organizado y agregados comentarios.

--- cliente.js
+++ cliente.js
@@ -1,96 +1,132 @@
+//evento al cargar la pagina
 window.addEventListener("load", function(event) {
 
+	//obtiene los elementos del formulario de chat
 	var status = document.getElementById("status");
 	var url = document.getElementById("url");
+	var userName = document.getElementById("userName");
+	var sendUserName = document.getElementById("sendUserName");
 	var open = document.getElementById("open");
 	var close = document.getElementById("close");
-	var sendMessage = document.getElementById("sendMessage");
-	var sendUserName = document.getElementById("sendUserName");
+	var chat = document.getElementById("chat");
 	var message = document.getElementById("message");
+	var sendMessage = document.getElementById("sendMessage");
+	//variable para almacenar el socket
 	var socket;
+	//pone el estado inicial de cada elemento de la interfaz
 	status.textContent = "Desconectado";
 	var page = document.createElement('a');
 	page.href = window.location.href;
+	//define la url del servidor como la hostname de la pagina y el puerto definido 8080 del ws
 	url.value = "ws://" + page.hostname + ":8080";
 	close.disabled = true;
-	sendMessage.disabled = true;
 	sendUserName.disabled = true;
-	message.disabled = true;
 	chat.disabled = true;
-	// Create a new connection when the Connect button is clicked              
+	message.disabled = true;
+	sendMessage.disabled = true;
+
+	//evento al presionar el boton de conectar         
 	open.addEventListener("click", function(event) {
+		//desabilita el boton para no permitir otra conexion simultanea
 		open.disabled = true;
+		//crea un socket con el servidor de ws con el protocolo echo-protocol 
+		//y envia como parametro el nombre de usuario (puede estar vacio)
 		socket = new WebSocket(url.value + "/" + userName.value, "echo-protocol");
+
+		/*A CONTINUACION DEFINE LOS EVENTOS QUE TENDRA EL SOCKET CREADO*/
+
+		//evento del socket cuando la conexion es establecida
 		socket.addEventListener("open", function(event) {
-			close.disabled = false;
+			//habilita los elementos del formulario de chat
 			sendUserName.disabled = false;
-			sendMessage.disabled = false;
-			message.disabled = false;
+			close.disabled = false;
 			chat.disabled = false;
+			message.disabled = false;
+			sendMessage.disabled = false;
+			//indica el estado conectado en color verde
 			status.textContent = "Conectado";
 			status.style.color = "green";
 		});
 
-		// Display messages received from the server
+		//evento del socket cuando se recibe un mensaje del servidor
 		socket.addEventListener("message", function(event) {
-			var chat = document.getElementById("chat")
+			//agrega al chat el mensaje recibido (formato = "user: mensaje")
 			chat.value = chat.value + "\n" + event.data;
+			//mueve el scroll del chat hasta el tope inferior
 			chat.scrollTop = chat.scrollHeight;
 		});
 
-		// Display any errors that occur
+		//evento del socket cuando ocurre un error de conexion
 		socket.addEventListener("error", function(event) {
-			message.textContent = "Error: " + event;
+			alert("Error al conectar con el servidor.");
 		});
 
+		//evento del socket cuando se cierra la conexion
 		socket.addEventListener("close", function(event) {
+			//habilita el boton para conectarse
 			open.disabled = false;
+			//indica el estado desconectado en color gris
 			status.textContent = "Desconectado";
 			status.style.color = "gray";
 		});
 
 	});
 
-	// Close the connection when the Disconnect button is clicked          
+	//evento al presionar el boton de desconectar     
 	close.addEventListener("click", function(event) {
-		close.disabled = true;
+		//deshabilita los elementos del formulario de chat
 		sendUserName.disabled = true;
-		sendMessage.disabled = true;
-		message.disabled = true;
+		close.disabled = true;
 		chat.disabled = true;
-		open.disabled = false;
+		message.disabled = true;
+		sendMessage.disabled = true;
+		//reinicia los valores alamcenados en el chat y la casilla de mensaje
 		message.value = "";
 		chat.value = "Conversación...";
+		//cierra el socket
 		socket.close();
 	});
 
-	// sendMessage text to the server when the sendMessage button is clicked          
+	//evento al presionar el boton de enviar
 	sendMessage.addEventListener("click", function(event) {
+		//verifica que el mensaje no este vacio
 		if (message.value != "") {
-			var msg = message.value;
-			socket.send("msg" + msg);
+			//envia a el mensaje con el socket
+			//utiliza la palabra msg para identificar que es un mensaje de chat
+			socket.send("msg" + message.value);
+			//borra el contenido de la casilla de mensaje
 			message.value = "";
 		}
 	});
 
+	//evento al presionar un tecla en la casilla de mensaje
 	message.addEventListener("keydown", function(event) {
+		//verifica si la tecla presionada fue ENTER y no se presiona SHIFT
 		if (event.keyCode === 13 && !event.shiftKey) {
+			//anula el enter que se escribiria
 			event.preventDefault();
+			//llama al evento de presionar sobre el boton de enviar
 			sendMessage.click();
 		}
 	});
 
-	//sendUserName text to the server when the sendUserName button is clicked          
+	//evento al presionar el boton de cambiar nombre
 	sendUserName.addEventListener("click", function(event) {
-		var msg = document.getElementById("userName").value;
-		if (msg != "") {
-			socket.send("name" + msg);
+		//verifica que el nombre no este vacio
+		if (userName.value != "") {
+			//envia a el nombre con el socket
+			//utiliza la palabra name para identificar que es un nombre de usuario
+			socket.send("name" + userName.value);
 		}
 	});
 
+	//evento al presionar un tecla en la casilla de nombre
 	userName.addEventListener("keydown", function(event) {
+		//verifica si la tecla presionada fue ENTER y no se presiona SHIFT
 		if (event.keyCode === 13 && !event.shiftKey) {
+			//anula el enter que se escribiria
 			event.preventDefault();
+			//llama al evento de presionar sobre el boton de cambiar nombre
 			sendUserName.click();
 		}
 	});
--- servidor.js
+++ servidor.js
@@ -1,88 +1,109 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
+//crea el objeto server que se pondra a la escucha
 var server = http.createServer(function(request, response) {
-	console.log((new Date()) + ' Received request for ' + request.url);
+	console.log((new Date()) + " Recibe conexion de " + request.url);
 	response.writeHead(404);
 	response.end();
 });
 
+//pone al servidor a escuchar en el puerto 8080
 server.listen(8080, function() {
-	console.log((new Date()) + ' Server is listening on port 8080');
+	console.log((new Date()) + " El servidor esta escuchando en el puerto 8080");
 });
 
-wsServer = new WebSocketServer({
+//crea el objeto ws
+var wsServer = new WebSocketServer({
 	httpServer: server,
-	// You should not use autoAcceptConnections for production
-	// applications, as it defeats all standard cross-origin protection
-	// facilities built into the protocol and the browser. You should
-	// *always* verify the connection's origin and decide whether or not
-	// to accept it.
 	autoAcceptConnections: false
 });
 
+//funcion que verifica si un cliente esta restringido (no utilizado)
 function originIsAllowed(origin) {
-	// put logic here to detect whether the specified origin is allowed.
+	//debido a que no se restringen clientes, no se realiza ningun tratamiento
 	return true;
 }
 
-//guardamos los clientes en un arreglo. Esta variable es global
+//guardamos los clientes en un arreglo
 var clientes = [];
 var cantUsers = 0;
 
 wsServer.on("request", function(request) {
-	if (!originIsAllowed(request.origin)) { // Make sure we only accept requests from an allowed origin
+	//verifica si la conexion proviene de un cliente restringido
+	if (!originIsAllowed(request.origin)) {
+		//rechaza la conexion
 		request.reject();
-		console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+		console.log((new Date()) + " La conexion que proviene de " + request.origin + ' fue rechazada.');
+		//sale del request
 		return;
 	}
 
+	//acepta la conexion utilizando el protocolo echo-protocol
 	var connection = request.accept('echo-protocol', request.origin);
-	console.log((new Date()) + ' Connection accepted.');
+	console.log((new Date()) + " La conexion que proviene de " + request.origin + " fue aceptada.");
 
-	//si la conexion es aceptada agregamos el cliente a la lista de clientes
+	//agrega la nueva conexion aceptada al arreglo de clientes
 	clientes.push(connection);
+	//parsea el url del request para obtener el nombre de usuario
 	var url = require('url').parse(request.httpRequest.url);
 	var param = url.pathname.substring(1);
-	var userName = "Usuario" + cantUsers;
+	var userName;
+	//verifica si se envio un parametro con el nombre de usuario
 	if (param != "") {
+		//setea el nombre de usuario con el parametro enviado
 		userName = param;
+	} else {
+		//setea un nombre de usuario generico
+		userName = "Usuario" + cantUsers;
 	}
-	
-	connection.id = cantUsers; //agregar un id unico al cliente para identificarlo univocamente.
+
+	//agregar un id unico al cliente para identificarlo univocamente.
+	connection.id = cantUsers;
 	cantUsers++;
-	//avisar en el chat la conexion del usuario
+
+	//avisar en el chat de cada usuario la conexion del nuevo usuario
 	for (var i = 0; i < clientes.length; i++) {
 		clientes[i].sendUTF("El usuario " + userName + " entró a al chat.");
 	}
 
+	//evento cuando el cliente envia un mensaje al servidor
 	connection.on("message", function(message) {
+		//verifica que el mensaje provenga de un mensaje de chat
 		if (message.utf8Data.substr(0, 3) == "msg") {
-			//enviar el msj a todos los clientes
+			//pasea para obtener el mensaje
 			var msg = message.utf8Data.substring(3);
+			//envia el mensaje de chat a todos los clientes
 			for (var i = 0; i < clientes.length; i++) {
 				clientes[i].sendUTF(userName + ": " + msg);
 			}
+			//verifica que el mensaje provenga de un cambio de nombre
 		} else if (message.utf8Data.substr(0, 4) == "name") {
+			//pasea para obtener el mensaje
 			var name = message.utf8Data.substring(4);
-			//avisar del cambio de nombre al resto de los usuarios
+			//evnia el mensaje de cambio de nombre a todos los clientes
 			for (var i = 0; i < clientes.length; i++) {
 				clientes[i].sendUTF(userName + " cambió su nombre a: " + name);
 			}
+			//cambia su nombre por el recibido
 			userName = name;
 		}
 	});
 
+	//evento cuando el cliente se desconecta del servidor
 	connection.on("close", function(reasonCode, description) {
-		//eliminar al cliente de la lista de clientes
-		var nro;
+		var nroCliente;
+		//busca al cliente en el arreglo y avisa a los demas de su desconexion
 		for (var i = 0; i < clientes.length; i++) {
 			if (connection.id == clientes[i].id) {
-				nro = i;
+				nroCliente = i;
 			}
+			//envia el mensaje de desconexion al resto de los clientes
 			clientes[i].sendUTF("El usuario " + userName + " se desconectó del chat.");
 		}
-		clientes.splice(nro, 1);
-		console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' ' + connection.id + ' disconnected.');
+		//elimina al cliente del arreglo de clientes
+		clientes.splice(nroCliente, 1);
+		console.log((new Date()) + " El cliente con id " + connection.id + ", nombre " + userName + " e ip " + connection.remoteAddress + " se ha desconectado.");
 	});
+
 });
